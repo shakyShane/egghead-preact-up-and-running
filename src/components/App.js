2d9782c9f9388a7e827f614a4eb867a4613e95ae
Shane Osbourne <shane.osbourne8@gmail.com>
2017-04-01 21:55:15 +0100

    Installing development tools for Preact

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,13 @@
 import { h, Component } from 'preact';
-import User from './User';
 
 export class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      user: null,
-      loading: true
-    }
-  }
-  componentDidMount() {
-    fetch(this.props.config.urls.user)
-        .then(resp => resp.json())
-        .then(user => {
-          this.setState({
-            user,
-            loading: false
-          });
-        })
-        .catch(err => console.error(err));
-  }
-  render({config}, {loading, user}) {
+  render(props, { text = '' }) {
     return (
-      <div class="app">
-        {loading
-          ? <p>Fetching {config.urls.user}</p>
-          : <User image={user.avatar_url}
-                  name={user.name} />
-        }
+      <div>
+        <input type="text" value={text} onInput={this.linkState('text')}/>
+        <pre><code>{JSON.stringify(this.state, null, 2)}</code></pre>
       </div>
-    );
+    )
   }
 }
 
