c85ad5f97143e80bf600f6d5b6b6dc7446d398dc
Shane Osbourne <shane.osbourne8@gmail.com>
2017-04-01 12:29:35 +0100

    Hello World with Preact

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+
+export function App () {
+    return (
+        <div class="app">
+            <h1>Hello world!</h1>
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('Hello!');
+import { h, render } from 'preact';
+import App from './components/App';
+
+render(<App />, document.querySelector('main'));
