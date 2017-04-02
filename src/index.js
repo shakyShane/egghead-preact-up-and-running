7788621e05f66c7ad5023c4aa7c1978030f3c670
Shane Osbourne <shane.osbourne8@gmail.com>
2017-04-02 21:04:17 +0100

    Using Redux Dev-tools with Preact

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import { h, render } from 'preact';
 import { Provider } from 'preact-redux';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import reducer from './reducer';
 import App from './components/App';
 
-const store = createStore(reducer, {
+const initialState = {
   loading: true,
   user: null
-}, applyMiddleware(thunk));
+};
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+          // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const store = createStore(reducer, initialState, composeEnhancers(applyMiddleware(thunk)));
 
 render((
     <div>
